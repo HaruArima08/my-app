2b568b04525c1820369e005876c9227025a610e3
HaruArima08 <uharubxxcd@gmail.com>
2025-05-15 13:29:05 +0900

    修正:SSGによるページの実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import React from "react";
+import styles from "../styles/Home.module.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
